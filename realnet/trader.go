bf8d37308de7b821b1b26f38ec8ec1519546b390
huoxuhuoxu <530607199@qq.com>
2019-05-27 21:50:27 +0800

    调整

--- realnet/trader.go
+++ realnet/trader.go
@@ -361,11 +361,11 @@ func (self *Trader) getWallet() {
 // 策略 - 计算根据持仓计算合理价格
 func (self *Trader) calculateReasonablePrice() (*PlaceOrderParams, *PlaceOrderParams) {
 	var (
-		bidPrice, askPrice, bidAmount, askAmount float64
+		bidPrice, askPrice, bidAmount, askAmount, middlePrice, reasonablePrice float64
 	)
 	self.ProcessLock.RLock()
+	middlePrice = (self.Depth.Buy + self.Depth.Sell) / 2
 
-	var reasonablePrice float64
 	if len(self.OrderBook.Asks) >= 10 && len(self.OrderBook.Bids) >= 10 {
 		var t, t2 float64
 		for _, ask := range self.OrderBook.Asks {
@@ -376,10 +376,11 @@ func (self *Trader) calculateReasonablePrice() (*PlaceOrderParams, *PlaceOrderPa
 			t += bid.Amount * bid.Price
 			t2 += bid.Amount
 		}
-		reasonablePrice = math.Ceil(t / t2)
+		reasonablePrice = t / t2
+		reasonablePrice = math.Ceil(reasonablePrice + (middlePrice - reasonablePrice))
 		self.Output.Info("合理价格", reasonablePrice)
 	} else {
-		reasonablePrice = math.Floor((self.Depth.Buy+self.Depth.Sell)/2 + 0.5)
+		reasonablePrice = math.Floor(middlePrice + 0.5)
 		self.Output.Info("中间价", reasonablePrice)
 	}
 
